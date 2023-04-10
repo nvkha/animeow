60a0ca51c70bfa31cc9a7ca1c5973ccf6230ec54
Kha Nguyen <nvkha98@gmail.com>
2023-04-10 12:35:38 +0700

    Fix somg bugs

--- public/js/swiper-slide.js
+++ public/js/swiper-slide.js
@@ -1,1 +1,1 @@
-const swiper=new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},autoplay:{delay:1e4,disableOnInteraction:!1}});
+const swiper=new Swiper(".swiper",{allowTouchMove:!1,loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},autoplay:{delay:1e4,disableOnInteraction:!1}});
