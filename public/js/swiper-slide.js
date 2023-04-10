53d28ec6e86e6c5ef1a9bdc62ab8eb7ac30b12b5
Kha Nguyen <nvkha98@gmail.com>
2023-04-10 11:53:43 +0700

    Fix some bugs

--- public/js/swiper-slide.js
+++ public/js/swiper-slide.js
@@ -1,1 +1,1 @@
-const swiper=new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},autoplay:{delay:5e3,disableOnInteraction:!1}});
+const swiper=new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},autoplay:{delay:1e4,disableOnInteraction:!1}});
