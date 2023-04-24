6eeed318c183eb496947a44b962389a36bcc6546
Kha Nguyen <nvkha98@gmail.com>
2023-04-24 10:28:59 +0700

    Fix bugs

--- public/js/player.js
+++ public/js/player.js
@@ -1,1 +1,1 @@
-videojs("player",{fluid:!0,controls:!0,autoplay:!1,preload: "none",notSupportedMessage:"Tập phim n\xe0y đ\xe3 bị lỗi! Bạn vui l\xf2ng th\xf4ng b\xe1o cho Admin nh\xe9! Thank you <3"});
+videojs("player",{fluid:!0,controls:!0,autoplay:!1,notSupportedMessage:"Tập phim n\xe0y đ\xe3 bị lỗi! Bạn vui l\xf2ng th\xf4ng b\xe1o cho Admin nh\xe9! Thank you <3"});
