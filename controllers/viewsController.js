d956ffd1c6a358370ab5694b91beb63d398b3b25
Kha Nguyen <nvkha98@gmail.com>
2023-05-06 02:01:01 +0700

    Fix some bugs

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -178,7 +178,7 @@ exports.getAnime = async (req, res, next) => {
             url: req.protocol + '://' + req.hostname + req.originalUrl,
             description: anime.otherTitle,
             keywords: `${anime.title},${anime.title} tap ${episodeNum}`,
-            image: 'https://ik.imagekit.io/3q7pewvsl/imgur/tr:w-300,h-425/' + anime.image
+            image: 'https://animeow.pro/imgur/' + anime.image
         }
 
         res.status(200).render('anime-watching', {
